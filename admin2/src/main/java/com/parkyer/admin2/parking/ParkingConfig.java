841ff808070b902dca00d740d43e5e197ddcdbbf
aortegaf <aortegaf@unal.edu.co>
2021-08-10 02:48:26 -0500

    new locations

--- admin2/src/main/java/com/parkyer/admin2/parking/ParkingConfig.java
+++ admin2/src/main/java/com/parkyer/admin2/parking/ParkingConfig.java
@@ -22,25 +22,25 @@ public class ParkingConfig {
             Parking p2 = new Parking(
                 4,
                 "null", 
-                "4.740596106774402",
-                "-74.03132287148797", 
-                "calle 141bis #15 - 45",
+                "4.6458605",
+                "-74.0795711", 
+                "el campin",
                 "carro"
             );
             Parking p3 = new Parking(
                 6,
                 "null", 
-                "4.740596106774402",
-                "-74.03132287148797", 
-                "calle 141bis #15 - 45",
+                "4.6381991",
+                "-74.0862404", 
+                "nacho",
                 "carro"
             );
             Parking p4 = new Parking(
                 9,
                 "null", 
-                "4.740596106774402",
-                "-74.03132287148797", 
-                "calle 50 #20 - 45",
+                "4.61213",
+                "-74.0769421", 
+                "la piscina 😏",
                 "moto"
             );
 
